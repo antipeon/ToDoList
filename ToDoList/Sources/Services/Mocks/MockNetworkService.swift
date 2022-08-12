84781a82df840a4f6b9b54aba2a90b170343b1a2
Samat Gaynutdinov <gainutdinov.sm@phystech.edu>
2022-08-12 23:39:59 +0300

    start write networkService

--- ToDoList/Sources/Services/Mocks/MockNetworkService.swift
+++ ToDoList/Sources/Services/Mocks/MockNetworkService.swift
@@ -8,41 +8,112 @@
 import Foundation
 
 final class MockNetworkService: NetworkService {
-    let model: ToDoListModel
 
-    init() throws {
-        try model = ToDoListModel()
-    }
+    // MARK: - Private vars
+    private let queriesQueue = DispatchQueue(label: "queriesQ", attributes: .concurrent)
 
+    //
+
+    // MARK: API
     func getAllToDoItems(completion: @escaping (Result<[ToDoItem], Error>) -> Void) {
-        let timeout = TimeInterval.random(in: 1..<3)
-        DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
-            completion(.success(self.model.items))
+        queriesQueue.asyncAfter(deadline: .now() + timeout()) { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            let data = self.performServerRequest()
+            let json =  self.parseToJson(data: data)
+
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {
+                    return
+                }
+
+                completion(.success(self.jsonToItems(json: json)))
+            }
         }
     }
 
     func editToDoItem(_ item: ToDoItem, completion: @escaping (Result<ToDoItem, Error>) -> Void) {
-        let timeout = TimeInterval.random(in: 1..<3)
-        DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
-            do {
-                try self.model.addItem(item)
+        queriesQueue.asyncAfter(deadline: .now() + timeout(), flags: .barrier) { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            let data = self.performServerRequest()
+            let json =  self.parseToJson(data: data)
+
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {
+                    return
+                }
+
+                guard let item = self.jsonToItem(json: json) else {
+                    completion(.failure(NetworkServiceError.failEditItem))
+                    return
+                }
+
                 completion(.success(item))
-            } catch {
-                completion(.failure(NetworkServiceError.failEditItem))
             }
         }
     }
 
     func deleteToDoItem(at id: String, completion: @escaping (Result<ToDoItem, Error>) -> Void) {
-        let timeout = TimeInterval.random(in: 1..<3)
-        DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
-            do {
-                let item = ToDoItem(withId: id)
-                try self.model.deleteItem(item)
-                completion(.success(item))
-            } catch {
-                completion(.failure(NetworkServiceError.failDeleteItem))
+        queriesQueue.asyncAfter(deadline: .now() + timeout(), flags: .barrier) { [weak self] in
+            guard let self = self else {
+                return
             }
+
+            let data = self.performServerRequest()
+            let json =  self.parseToJson(data: data)
+
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {
+                    return
+                }
+
+                guard let item = self.jsonToItem(json: json) else {
+                    completion(.failure(NetworkServiceError.failEditItem))
+                    return
+                }
+
+                completion(.success(item))
+            }
+        }
+    }
+
+    // MARK: - Stubs
+    private func performServerRequest() -> Data {
+        Data()
+    }
+
+    private func parseToJson(data: Data) -> Any {
+        return 0
+    }
+
+    private func jsonToItems(json: Any) -> [ToDoItem] {
+        Constants.MockData.items
+    }
+
+    private func jsonToItem(json: Any) -> ToDoItem? {
+        Constants.MockData.items.first
+    }
+
+    // MARK: - Helpers
+    private func timeout() -> TimeInterval {
+        TimeInterval.random(in: Constants.timeoutLowerBound..<Constants.timeoutHigherBound)
+    }
+
+    private enum Constants {
+        static let filename = "queries"
+        static let timeoutLowerBound: TimeInterval = 1
+        static let timeoutHigherBound: TimeInterval = 3
+
+        enum MockData {
+            static let items = [
+                ToDoItem(withId: "123"),
+                ToDoItem(withId: "qwerty")
+            ]
         }
     }
 }
